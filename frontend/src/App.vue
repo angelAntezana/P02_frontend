59d88c1900d7892fa22d778c93d828897613518c
angelAntezana <angel2estevan@gmail.com>
2023-11-04 21:10:38 +0100

    Prueba de concepto con options api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,40 @@
 <template>
-  <router-view />
+  <template v-if="autenticado">
+    <router-view />
+  </template>
+  <template v-else>
+    <div>
+      <h1>hola mundo</h1>
+    </div>
+  </template>
+  <button @click="mostrarApp">Mostrar app</button>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
+import { defineComponent } from 'vue';
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      autenticado: false,
+      count:0
+    }
+  },
+
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+  methods: {
+    mostrarApp() {
+      this.autenticado=!this.autenticado;
+      this.count++;
+      console.log(`The count is ${this.count}.`)
+
+    }
+  },
+
+
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
+  }
 })
 </script>
